73f3cf8b64600b4fe51e275061de2b46ae30ea21
Allan Ebdrup <aeb@e-conomic.com>
2015-09-05 23:07:39 +0200

    an actual war

--- index.js
+++ index.js
@@ -4,9 +4,16 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.use(express.static('public'));
+var colors = [0xefefef, 0x244f6a, 0xfeaa37, 0xe03e27];
+var colorIndex = Math.round(Math.random() * colors.length);
 
 io.on('connection', function(socket){
+	colorIndex = (colorIndex+1) % colors.length;
+	var color = colors[colorIndex];
 	socket.on('click', function(msg){
+		msg.color = color;
+		msg.size = 10;
+		console.log(msg);
 		io.emit('click', msg);
 	});
 });
