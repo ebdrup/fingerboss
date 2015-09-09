9258b862f76b0087f7cdd8352a363b3f3b087e7c
Allan Ebdrup <aeb@e-conomic.com>
2015-09-09 21:47:45 +0200

    use median dt

--- server.js
+++ server.js
@@ -14,21 +14,35 @@ var port = process.env.PORT || 7890;
 var colors = [0xefefef, 0x244f6a, 0xfeaa37, 0xe03e27];
 var colorIndex = Math.round(Math.random() * colors.length);
 
-io.on('connection', function(socket){
-	colorIndex = (colorIndex+1) % colors.length;
+io.on('connection', function (socket) {
+	colorIndex = (colorIndex + 1) % colors.length;
 	var color = colors[colorIndex];
-	socket.emit('start', { color:color, t:Date.now(), velocity:0.0002 });
-	socket.on('circle', function(c){
+	emit(socket, 'start', {color: color, t: Date.now(), velocity: 0.0002});
+	socket.on('circle', function (c) {
 		c.color = color;
 		c.t = Date.now();
-		console.log(c);
 		broadcast('circle', c);
 	});
 });
 
-function broadcast(type, msg){
-	if(!process.env.PORT){
-		return setTimeout(emit, 300);
+var LATENCY = 500;
+
+function emit(socket, type, msg){
+	if (!process.env.PORT) {
+		msg.t += LATENCY/2;
+		return setTimeout(emit, LATENCY);
+	}
+	return emit();
+
+	function emit() {
+		socket.emit(type, msg);
+	}
+}
+
+function broadcast(type, msg) {
+	if (!process.env.PORT) {
+		msg.t += LATENCY/2;
+		return setTimeout(emit, LATENCY);
 	}
 	return emit();
 
@@ -37,6 +51,6 @@ function broadcast(type, msg){
 	}
 }
 
-http.listen(port, function(){
+http.listen(port, function () {
 	console.log('listening on http://localhost:%s', port);
 });
