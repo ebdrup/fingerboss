8ef4a398ca7ee5f2f875f02b7beed649c5988eb0
Allan Ebdrup <aeb@e-conomic.com>
2015-09-19 22:31:46 +0200

    cleanup sounds for phone app

--- js/sfx.js
+++ js/sfx.js
@@ -1,37 +1,14 @@
-var SOUND = true;
 function sfx() {
-	var s = {
-		newCircle: new Howl({
-			urls: ['newConfirmedCircle.mp3'],
+	return ['newCircle', 'crash1', 'crash2', 'shrink', 'fingerboss', 'loose'].reduce(function(acc, key){
+		var exts = window.intel ? ['wav', 'mp3'] : ['mp3', 'wav'];
+		var urls = exts.map(function(ext){
+			return 'sound/' + key + '.' + ext;
+		});
+		var sound = new Howl({
+			urls: urls,
 			volume: 0.3
-		}),
-		crash1: new Howl({
-			urls: ['crash1.mp3'],
-			volume: 0.3
-		}),
-		crash2: new Howl({
-			urls: ['crash2.mp3'],
-			volume: 0.3
-		}),
-		shrink: new Howl({
-			urls: ['shrink.mp3'],
-			volume: 0.04
-		}),
-		fingerboss: new Howl({
-			urls: ['fingerboss.mp3'],
-			volume: 0.8
-		}),
-		loose: new Howl({
-			urls: ['loose.mp3'],
-			volume: 0.3
-		})
-	};
-	return Object.keys(s).reduce(function (acc, key) {
-		var sound = s[key];
+		});
 		acc[key] = function (volume) {
-			if (!SOUND) {
-				return;
-			}
 			if (volume) {
 				sound._volume = volume;
 			}
