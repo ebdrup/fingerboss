3c016d7838df75796e80d0f46833ccf42f5a130a
Allan Ebdrup <aeb@e-conomic.com>
2015-09-20 09:18:14 +0200

    a bit harder computer

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ function fire() {
 		return setTimeout(fire, TIMEOUT);
 	}
 	var s = Math.random() / 5;
-	var t = s * 6500 + 500;
+	var t = s * 5900 + 500;
 	var x, y;
 	if (lastCircle && Math.random() > 0.5) {
 		x = lastCircle.x;
