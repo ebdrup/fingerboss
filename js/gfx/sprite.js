4a7562367c7322b5fbb66aa9d261c4a33ad61cae
Allan Ebdrup <allan@878.dk>
2016-10-12 17:51:45 +0200

    power is lightning

--- js/gfx/sprite.js
+++ js/gfx/sprite.js
@@ -1,3 +1,4 @@
+var spriteCache = {};
 function sprite({type, size, x, y, color}) {
 	color = color || {
 			'ball': 0xffffff,
@@ -7,23 +8,42 @@ function sprite({type, size, x, y, color}) {
 	var height = world.renderer.view.height;
 	var width = world.renderer.view.width;
 	var s = (width + height) / 2;
-	var key = s + '_' + color;
-	var texture = world.textures[key];
-	if (!texture) {
-		var gfx = new PIXI.Graphics({cacheAsBitmap: true});
-		gfx.beginFill(color, 0.8);
-		gfx.drawCircle(0, 0, 0.1 * s);
-		world.textures[key] = texture = gfx.generateCanvasTexture(world.renderer);
+	var spriteKey = [type, size, x, y, color].join('_');
+	var sprite;
+	if(spriteCache[spriteKey]){
+		sprite = spriteCache[spriteKey];
+	} else {
+		var textureKey = [s, color, type].join('_');
+		var texture = world.textures[textureKey];
+		if (!texture) {
+			switch (type) {
+				case 'power':
+					world.textures[textureKey] = texture = new PIXI.Texture.fromImage('power.png');
+					break;
+				default:
+					var gfx = new PIXI.Graphics({cacheAsBitmap: true});
+					gfx.beginFill(color, 0.8);
+					gfx.drawCircle(0, 0, 0.1 * s);
+					world.textures[textureKey] = texture = gfx.generateCanvasTexture(world.renderer);
+			}
+		}
+		sprite = new PIXI.Sprite(texture);
+		// center the sprite's anchor point
+		sprite.anchor.x = 0.5;
+		sprite.anchor.y = 0.5;
+		var px = (x - state.pos.x) * width;
+		var py = (y - state.pos.y) * height;
+		sprite.position.x = px;
+		sprite.position.y = py;
+		sprite.width = sprite.height = size * s;
 	}
-	var sprite = new PIXI.Sprite(texture);
-	// center the sprite's anchor point
-	sprite.anchor.x = 0.5;
-	sprite.anchor.y = 0.5;
-	var px = (x - state.pos.x) * width;
-	var py = (y - state.pos.y) * height;
-	sprite.position.x = px;
-	sprite.position.y = py;
-	sprite.width = sprite.height = size * s;
-	sprite.color = color;
+	switch (type) {
+		case 'power':
+			sprite.alpha = 0.5 + (Math.random()*0.5);
+			sprite.rotation = (Math.random()-0.5)/6;
+			break;
+		default:
+			sprite.color = color;
+	}
 	return sprite;
 }
