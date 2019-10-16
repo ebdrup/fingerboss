c8b5ef6eac784dd62b7c9968692f4fa726fc19aa
Allan Ebdrup <allan@878.dk>
2019-10-16 12:32:47 +0200

    fix for latest node

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,49 @@
-const path = require('path');
-const gulp = require('gulp');
-const uglify = require('gulp-uglify');
-const sourcemaps = require('gulp-sourcemaps');
-const concat = require('gulp-concat');
-const addSrc = require('gulp-add-src');
-const browserify = require('gulp-browserify');
-const nodemon = require('gulp-nodemon');
-const babel = require('gulp-babel');
-const staticFiles = require('./staticFiles');
+const path = require("path");
+const gulp = require("gulp");
+const concat = require("gulp-concat");
+const nodemon = require("gulp-nodemon");
+const staticFiles = require("./staticFiles");
 
-gulp.task('compress', function () {
-	return gulp.src([
-		'js/**/*.js',
-	])
-		.pipe(sourcemaps.init())
-		.pipe(babel({
-			presets: ['es2015']
-		}))
-	//	.pipe(uglify())
-		.pipe(addSrc(staticFiles))
-		.pipe(concat('bundle.js'))
-		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest('public'));
+gulp.task("compress", function() {
+  return gulp
+    .src(staticFiles.concat(["js/**/*.js"]))
+    .pipe(concat("bundle.js"))
+    .pipe(gulp.dest("public"));
 });
 
-gulp.task('watch', function () {
-	gulp.watch('js/**/*.js', ['compress']);
+gulp.task("watch", function() {
+  gulp.watch("js/**/*.js", ["compress"]);
 });
 
-gulp.task('webserver', function () {
-	var stream = nodemon({
-		script: 'server.js',
-		ext: 'js html',
-		env: { 'NODE_ENV': 'development' },
-		ignore: ['public/bundle.*'],
-		tasks: function (changedFiles) {
-			var tasks = [];
-			console.log(changedFiles);
-			changedFiles && changedFiles.forEach && changedFiles.forEach(function (file) {
-				if (path.extname(file) === '.js' && !~tasks.indexOf('compress')){
-					tasks.push('compress');
-				}
-			});
-			return tasks;
-		}
-	});
-	stream
-		.on('restart', function () {
-			console.log('restarted!');
-		})
-		.on('crash', function() {
-			console.error('Application has crashed!\n');
-			stream.emit('restart', 1);  // restart the server in 1 seconds
-		});
+gulp.task("webserver", function() {
+  var stream = nodemon({
+    script: "server.js",
+    ext: "js html",
+    env: { NODE_ENV: "development" },
+    ignore: ["public/bundle.*"],
+    tasks: function(changedFiles) {
+      var tasks = [];
+      console.log(changedFiles);
+      changedFiles &&
+        changedFiles.forEach &&
+        changedFiles.forEach(function(file) {
+          if (path.extname(file) === ".js" && !~tasks.indexOf("compress")) {
+            tasks.push("compress");
+          }
+        });
+      return tasks;
+    }
+  });
+  stream
+    .on("restart", function() {
+      console.log("restarted!");
+    })
+    .on("crash", function() {
+      console.error("Application has crashed!\n");
+      stream.emit("restart", 1); // restart the server in 1 seconds
+    });
 });
 
-gulp.task('default', function () {
-	gulp.run(['compress', 'watch', 'webserver']);
+gulp.task("default", function() {
+  gulp.run(["compress", "watch", "webserver"]);
 });
